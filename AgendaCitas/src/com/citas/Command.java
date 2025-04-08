53e6c1efec71d9d0946fe175ab6ba7d0cb5b41c1
copper-san <danielmuscalu6@gmail.com>
2025-04-08 12:55:34 +0200

    Creada la interfaz con las funciones para los comandos

--- AgendaCitas/src/com/citas/Command.java
+++ AgendaCitas/src/com/citas/Command.java
@@ -1,5 +1,10 @@
 package com.citas;
 
+import java.util.List;
+
 public interface Command {
 
+	List<Appointment> seeDates(String clientId);
+	Boolean removeDate(String clientId, String dateTime);
+	Boolean addDate(Appointment appointment);
 }
